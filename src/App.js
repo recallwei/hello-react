2b7b1c0dea85a74e3b89bac5f9292220311799ce
Bruce Song <recall4056@gmail.com>
2022-06-23 23:50:44 +0800

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+function Container() {
+  return (
+    <div
+      style={{
+        backgroundColor: "grey",
+        position: "absolute",
+        top: "20%",
+        bottom: "20%",
+        left: "20%",
+        right: "20%",
+        margin: 0,
+      }}
+    ></div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <Container />
+    </header>
   );
 }
 
