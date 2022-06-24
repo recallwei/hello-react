006721a4626fdbf3ef5a6a69b5c09be28c4a2458
Bruce Song <recall4056@gmail.com>
2022-06-24 08:44:58 +0800

    Add a setState example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useState } from "react";
 function Container() {
+  const [array, setArray] = useState([]);
+
+  let changeData = () => {
+    let a = array;
+    let b = a[0];
+    setArray([{ ...b, color: "blue" }]);
+  };
   return (
     <div
       style={{
-        backgroundColor: "grey",
+        backgroundColor: array[0]?.color ? array[0]?.color : "grey",
         position: "absolute",
         top: "20%",
         bottom: "20%",
@@ -11,7 +19,30 @@ function Container() {
         right: "20%",
         margin: 0,
       }}
-    ></div>
+    >
+      {array[0]?.color} {array[0]?.number}
+      <button
+        onClick={() => {
+          setArray([{ color: "azure", number: 2 }]);
+        }}
+      >
+        Click here to make color azure
+      </button>
+      <button
+        onClick={() => {
+          setArray([{ color: "pink", number: 5 }]);
+        }}
+      >
+        Click here to make color pink
+      </button>
+      <button
+        onClick={() => {
+          changeData();
+        }}
+      >
+        Click here to make color blue
+      </button>
+    </div>
   );
 }
 
