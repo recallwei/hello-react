5af275d7c97c83505dce73ffb120c3baaf0fc580
Bruce Song <recall4056@gmail.com>
2022-06-26 01:14:48 +0800

    useEffect Test.

--- src/FirstLevelComponent.js
+++ src/FirstLevelComponent.js
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
 export default function FirstLevelComponent() {
   const [array, setArray] = useState([]);
-  useEffect(() => {}, []);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    printNum();
+  }, []);
+  function printNum() {
+    console.log(count);
+  }
   let changeData = () => {
     let a = array;
     let b = a[0];
@@ -17,30 +23,46 @@ export default function FirstLevelComponent() {
         left: "20%",
         right: "20%",
         margin: 0,
+        padding: 15,
       }}
     >
       {array[0]?.color} {array[0]?.number}
-      <button
-        onClick={() => {
-          setArray([{ color: "azure", number: 2 }]);
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridAutoRows: 50,
+          gap: 10,
         }}
       >
-        Click here to make color azure
-      </button>
-      <button
-        onClick={() => {
-          setArray([{ color: "pink", number: 5 }]);
-        }}
-      >
-        Click here to make color pink
-      </button>
-      <button
-        onClick={() => {
-          changeData();
-        }}
-      >
-        Click here to make color blue
-      </button>
+        <button
+          onClick={() => {
+            setArray([{ color: "azure", number: 2 }]);
+          }}
+        >
+          Click here to make color azure
+        </button>
+        <button
+          onClick={() => {
+            setArray([{ color: "pink", number: 5 }]);
+          }}
+        >
+          Click here to make color pink
+        </button>
+        <button
+          onClick={() => {
+            changeData();
+          }}
+        >
+          Click here to make color blue
+        </button>
+        <input
+          name="count"
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
