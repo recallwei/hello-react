fdf400d34271fee5981dceae0b8b13305981e913
Bruce Song <recall4056@gmail.com>
2022-06-25 19:13:25 +0800

    Add useEffect.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,10 @@
 import React from "react";
-import { useState } from "react";
-function Container() {
-  const [array, setArray] = useState([]);
-
-  let changeData = () => {
-    let a = array;
-    let b = a[0];
-    setArray([{ ...b, color: "blue" }]);
-  };
-  return (
-    <div
-      style={{
-        backgroundColor: array[0]?.color ? array[0]?.color : "grey",
-        position: "absolute",
-        top: "20%",
-        bottom: "20%",
-        left: "20%",
-        right: "20%",
-        margin: 0,
-      }}
-    >
-      {array[0]?.color} {array[0]?.number}
-      <button
-        onClick={() => {
-          setArray([{ color: "azure", number: 2 }]);
-        }}
-      >
-        Click here to make color azure
-      </button>
-      <button
-        onClick={() => {
-          setArray([{ color: "pink", number: 5 }]);
-        }}
-      >
-        Click here to make color pink
-      </button>
-      <button
-        onClick={() => {
-          changeData();
-        }}
-      >
-        Click here to make color blue
-      </button>
-    </div>
-  );
-}
+import FirstLevelComponent from "./FirstLevelComponent";
 
 function App() {
   return (
     <header>
-      <Container />
+      <FirstLevelComponent />
     </header>
   );
 }
